fb0f1803b4ba83311da22a2617faaa235752e556
TriForMine <quentin.nicolini@hotmail.fr>
2022-09-16 14:41:10 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,7 +93,7 @@ const Home: NextPage = () => {
                     variant="contained"
                     onClick={() => {
                         setCookie('ine', INE?.toString(), {
-                            maxAge: 30 * 24 * 60 * 60 * 1000
+                            maxAge: 30 * 24 * 60 * 60 * 1000,
                         })
                     }}
                     disabled={INE?.toString().length !== 8}
